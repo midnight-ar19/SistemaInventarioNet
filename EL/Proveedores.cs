6674fd9fec2d5572ffd8d2440c029e5b4860f76b
zury <cr0602032024@unab.edu.sv>
2025-03-25 12:38:05 -0600

    Agregando clase proveedores - Zury Castillo

---
+++ EL/Proveedores.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    [Table("Proveedores")]
+    public class Proveedores
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Idproveedores { get; set; }
+
+        [Required, StringLength(150)]
+        [Column(name: "nombre_proveedor")]
+        public string NombreProveedor { get; set; }
+
+        [Required, StringLength(20)]
+        [Column(name: "telefono")]
+        public string TelefonoProveedor { get; set; }
+
+        [Required, StringLength(100)]
+        [Column(name: "email")]
+        public string emailProveedor { get; set; }
+
+
+
+    }
+}
