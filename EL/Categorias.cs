56fb1ddeb8901e9cc78b2e399ba14d5959cd3bfc
midnight <alanalvarez19r@gmail.com>
2025-03-24 18:12:53 -0600

    Agregando categorias

---
+++ EL/Categorias.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required,StringLength(100)]
+        [Column(name: "nombre_categoria")]
+        public string NombreCategoria { get; set; }
+    }
+}
