2f1038f55f8d94e357dba2ad580e92c2c45280ff
Jacqueline <bl0619032024@unab.edu.sv>
2025-03-24 18:22:22 -0600

    Agregando la clase marcas - Jacqueline Bonilla

---
+++ EL/Marca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    [Table("Marcas")]
+    public class Marca
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        [Column(name: "nombre")]
+        public string Nombre { get; set; }
+    }
+}
