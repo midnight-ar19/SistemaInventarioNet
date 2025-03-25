6a5eb02bfc5c71c6bee5a73ed83c082fec7bafcd
Elizabyt <cm02870320@unab.edu.sv>
2025-03-25 15:57:29 -0600

    Agredando Clase Permiso-Aymie Chicas

---
+++ EL/Permisos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    [Table("Pernmisos")]
+    public class Permisos
+    {
+        [key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPermiso { get; set; }
+
+        [Required, StringLength(100)]
+        [Column(name: "nombre_permiso")]
+    }
+}
