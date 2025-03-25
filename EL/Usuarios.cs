7bc4d200163ff1343d96f8f0b79a08a66803fbd7
susanalopez90 <sire0823@gimail.com>
2025-03-24 19:32:06 -0600

    Agregando la clase Usuarios-Susana López

---
+++ EL/Usuarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Usuarios
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string NombreUsuario { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Contrasena { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [EmailAddress]
+    public string Correo { get; set; }
+
+    [StringLength(20)]
+    public string Telefono { get; set; }
+
+    public DateTime FechaRegistro { get; set; } = DateTime.Now;
+
+    [StringLength(10)]
+    public string Estado { get; set; } = "Activo";
+}
