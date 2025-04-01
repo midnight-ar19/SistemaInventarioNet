ba254aeb3b32b43cf86b52a062461f231c8ef3ca
midnight <alanalvarez19r@gmail.com>
2025-03-31 18:23:22 -0600

    Se agrego datagridview - Alan Alvarez

--- GUI/Producto/FormProducto.Designer.cs
+++ GUI/Producto/FormProducto.Designer.cs
@@ -28,19 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(37, 415);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.RowHeadersWidth = 51;
+            this.dgvProductos.RowTemplate.Height = 24;
+            this.dgvProductos.Size = new System.Drawing.Size(1372, 274);
+            this.dgvProductos.TabIndex = 0;
             // 
             // FormProducto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1449, 720);
+            this.Controls.Add(this.dgvProductos);
             this.Name = "FormProducto";
             this.Text = "FormProducto";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvProductos;
     }
 }
