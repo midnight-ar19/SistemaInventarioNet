526aab268a10ddcd33fa1eac091d48d9f0e3d3f6
Reeeeh <eh0093032024@unab.edu.sv>
2025-03-31 23:35:36 -0600

    agregando comboBox - Raul Escobar

--- GUI/Producto/FormProducto.Designer.cs
+++ GUI/Producto/FormProducto.Designer.cs
@@ -36,6 +36,9 @@
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnEditar = new System.Windows.Forms.Button();
+            this.comboCategoria = new System.Windows.Forms.ComboBox();
+            this.comboProveedor = new System.Windows.Forms.ComboBox();
+            this.comboMarca = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,46 +46,52 @@
             // 
             this.dgvProductos.BackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProductos.Location = new System.Drawing.Point(37, 415);
+            this.dgvProductos.Location = new System.Drawing.Point(28, 337);
+            this.dgvProductos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvProductos.Name = "dgvProductos";
             this.dgvProductos.RowHeadersWidth = 51;
             this.dgvProductos.RowTemplate.Height = 24;
-            this.dgvProductos.Size = new System.Drawing.Size(1372, 274);
+            this.dgvProductos.Size = new System.Drawing.Size(1029, 223);
             this.dgvProductos.TabIndex = 0;
             // 
             // txtDescripcionProducto
             // 
-            this.txtDescripcionProducto.Location = new System.Drawing.Point(155, 133);
+            this.txtDescripcionProducto.Location = new System.Drawing.Point(116, 108);
+            this.txtDescripcionProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtDescripcionProducto.Name = "txtDescripcionProducto";
-            this.txtDescripcionProducto.Size = new System.Drawing.Size(227, 22);
+            this.txtDescripcionProducto.Size = new System.Drawing.Size(171, 20);
             this.txtDescripcionProducto.TabIndex = 2;
             // 
             // txtPrecioCompraProducto
             // 
-            this.txtPrecioCompraProducto.Location = new System.Drawing.Point(155, 198);
+            this.txtPrecioCompraProducto.Location = new System.Drawing.Point(116, 161);
+            this.txtPrecioCompraProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPrecioCompraProducto.Name = "txtPrecioCompraProducto";
-            this.txtPrecioCompraProducto.Size = new System.Drawing.Size(227, 22);
+            this.txtPrecioCompraProducto.Size = new System.Drawing.Size(171, 20);
             this.txtPrecioCompraProducto.TabIndex = 3;
             // 
             // txtPrecioVentaProducto
             // 
-            this.txtPrecioVentaProducto.Location = new System.Drawing.Point(155, 271);
+            this.txtPrecioVentaProducto.Location = new System.Drawing.Point(116, 220);
+            this.txtPrecioVentaProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPrecioVentaProducto.Name = "txtPrecioVentaProducto";
-            this.txtPrecioVentaProducto.Size = new System.Drawing.Size(227, 22);
+            this.txtPrecioVentaProducto.Size = new System.Drawing.Size(171, 20);
             this.txtPrecioVentaProducto.TabIndex = 4;
             // 
             // txtNombreProducto
             // 
-            this.txtNombreProducto.Location = new System.Drawing.Point(155, 64);
+            this.txtNombreProducto.Location = new System.Drawing.Point(116, 52);
+            this.txtNombreProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNombreProducto.Name = "txtNombreProducto";
-            this.txtNombreProducto.Size = new System.Drawing.Size(227, 22);
+            this.txtNombreProducto.Size = new System.Drawing.Size(171, 20);
             this.txtNombreProducto.TabIndex = 5;
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(96, 319);
+            this.btnGuardar.Location = new System.Drawing.Point(72, 259);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(81, 38);
+            this.btnGuardar.Size = new System.Drawing.Size(61, 31);
             this.btnGuardar.TabIndex = 6;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
@@ -90,27 +99,56 @@
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(301, 319);
+            this.btnEliminar.Location = new System.Drawing.Point(226, 259);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(81, 38);
+            this.btnEliminar.Size = new System.Drawing.Size(61, 31);
             this.btnEliminar.TabIndex = 7;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
             // 
             // btnEditar
             // 
-            this.btnEditar.Location = new System.Drawing.Point(199, 319);
+            this.btnEditar.Location = new System.Drawing.Point(149, 259);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(81, 38);
+            this.btnEditar.Size = new System.Drawing.Size(61, 31);
             this.btnEditar.TabIndex = 8;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
             // 
+            // comboCategoria
+            // 
+            this.comboCategoria.FormattingEnabled = true;
+            this.comboCategoria.Location = new System.Drawing.Point(743, 85);
+            this.comboCategoria.Name = "comboCategoria";
+            this.comboCategoria.Size = new System.Drawing.Size(223, 21);
+            this.comboCategoria.TabIndex = 9;
+            // 
+            // comboProveedor
+            // 
+            this.comboProveedor.FormattingEnabled = true;
+            this.comboProveedor.Location = new System.Drawing.Point(743, 125);
+            this.comboProveedor.Name = "comboProveedor";
+            this.comboProveedor.Size = new System.Drawing.Size(223, 21);
+            this.comboProveedor.TabIndex = 10;
+            // 
+            // comboMarca
+            // 
+            this.comboMarca.FormattingEnabled = true;
+            this.comboMarca.Location = new System.Drawing.Point(743, 161);
+            this.comboMarca.Name = "comboMarca";
+            this.comboMarca.Size = new System.Drawing.Size(223, 21);
+            this.comboMarca.TabIndex = 11;
+            // 
             // FormProducto
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1449, 720);
+            this.ClientSize = new System.Drawing.Size(1087, 585);
+            this.Controls.Add(this.comboMarca);
+            this.Controls.Add(this.comboProveedor);
+            this.Controls.Add(this.comboCategoria);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnGuardar);
@@ -119,6 +157,7 @@
             this.Controls.Add(this.txtPrecioCompraProducto);
             this.Controls.Add(this.txtDescripcionProducto);
             this.Controls.Add(this.dgvProductos);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormProducto";
             this.Text = "FormProducto";
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
@@ -137,5 +176,8 @@
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.ComboBox comboCategoria;
+        private System.Windows.Forms.ComboBox comboProveedor;
+        private System.Windows.Forms.ComboBox comboMarca;
     }
 }
