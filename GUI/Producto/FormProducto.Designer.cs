d4c810e729af42ffc2b902670141d344893f2cf7
susanalopez90 <sire0823@gimail.com>
2025-03-31 19:23:02 -0600

    Se agregaron los textBox- Susana López

--- GUI/Producto/FormProducto.Designer.cs
+++ GUI/Producto/FormProducto.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.txtDescripcionProducto = new System.Windows.Forms.TextBox();
+            this.txtPrecioCompraProducto = new System.Windows.Forms.TextBox();
+            this.txtPrecioVentaProducto = new System.Windows.Forms.TextBox();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,21 +47,58 @@
             this.dgvProductos.Size = new System.Drawing.Size(1372, 274);
             this.dgvProductos.TabIndex = 0;
             // 
+            // txtDescripcionProducto
+            // 
+            this.txtDescripcionProducto.Location = new System.Drawing.Point(155, 133);
+            this.txtDescripcionProducto.Name = "txtDescripcionProducto";
+            this.txtDescripcionProducto.Size = new System.Drawing.Size(227, 22);
+            this.txtDescripcionProducto.TabIndex = 2;
+            // 
+            // txtPrecioCompraProducto
+            // 
+            this.txtPrecioCompraProducto.Location = new System.Drawing.Point(155, 198);
+            this.txtPrecioCompraProducto.Name = "txtPrecioCompraProducto";
+            this.txtPrecioCompraProducto.Size = new System.Drawing.Size(227, 22);
+            this.txtPrecioCompraProducto.TabIndex = 3;
+            // 
+            // txtPrecioVentaProducto
+            // 
+            this.txtPrecioVentaProducto.Location = new System.Drawing.Point(155, 271);
+            this.txtPrecioVentaProducto.Name = "txtPrecioVentaProducto";
+            this.txtPrecioVentaProducto.Size = new System.Drawing.Size(227, 22);
+            this.txtPrecioVentaProducto.TabIndex = 4;
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Location = new System.Drawing.Point(155, 64);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(227, 22);
+            this.txtNombreProducto.TabIndex = 5;
+            // 
             // FormProducto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1449, 720);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.txtPrecioVentaProducto);
+            this.Controls.Add(this.txtPrecioCompraProducto);
+            this.Controls.Add(this.txtDescripcionProducto);
             this.Controls.Add(this.dgvProductos);
             this.Name = "FormProducto";
             this.Text = "FormProducto";
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.TextBox txtDescripcionProducto;
+        private System.Windows.Forms.TextBox txtPrecioCompraProducto;
+        private System.Windows.Forms.TextBox txtPrecioVentaProducto;
+        private System.Windows.Forms.TextBox txtNombreProducto;
     }
 }
