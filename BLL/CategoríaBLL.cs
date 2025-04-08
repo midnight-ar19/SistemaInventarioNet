d066a6c5ef4825cbe48dbfb9e149b1f04d93e053
susanalopez90 <sire0823@gimail.com>
2025-04-08 14:56:55 -0600

    Agregué las Clases CategoriaBLL y ProveedorBLL-Susana Lopez

---
+++ BLL/CategoríaBLL.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BLL
+{
+    public class CategoriaBLL
+    {
+        public static List<Categoria> ObtenerTodos()
+        {
+            using (var db = new AppDbContext())
+            {
+                return db.Categorias.ToList();
+            }
+        }
+    }
+}
---
+++ BLL/ProveedorBLL.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using EL;
+using System.Collections.Generic;
+using System;
+
+namespace BLL
+{
+    public class ProveedorBLL
+    {
+        public static List<Proveedor> ObtenerTodos()
+        {
+            using (var db = new AppDbContext())
+            {
+                return db.Proveedores.ToList();
+            }
+        }
+    }
+}
+
