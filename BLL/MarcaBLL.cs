9745b297b6f38399878286d86408d65d8ac92a21
midnight <alanalvarez19r@gmail.com>
2025-04-22 20:48:10 -0600

    Agregando MarcaBLL - Alan Alvarez

--- BLL/MarcaBLL.cs
+++ BLL/MarcaBLL.cs
@@ -3,21 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
+using EL;
 
 namespace BLL
 {
     public class MarcaBLL
     {
-        private MarcaBLL marcaBLL = new MarcaBLL();
+        private readonly MarcaDAL _marcaDAL;
 
-
-
-        public List<MarcaBLL> ObtenerTodos()
-
+        public MarcaBLL(MarcaDAL marcaDAL)
         {
+            _marcaDAL = marcaDAL;
+        }
 
-            return marcaBLL.ObtenerTodos();
+        // Obtener todas las marcas
+        public List<Marca> ObtenerMarcas()
+        {
+            return _marcaDAL.ObtenerMarcas();
+        }
 
+        // Agregar una nueva marca
+        public void AgregarMarca(Marca marca)
+        {
+            _marcaDAL.AgregarMarca(marca);
+        }
+
+        // Buscar una marca por ID
+        public Marca BuscarMarca(int id)
+        {
+            return _marcaDAL.BuscarMarca(id);
+        }
+
+        // Actualizar una marca
+        public void ActualizarMarca(Marca marca)
+        {
+            _marcaDAL.ActualizarMarca(marca);
+        }
+
+        // Eliminar una marca por ID
+        public void EliminarMarca(int id)
+        {
+            _marcaDAL.EliminarMarca(id);
         }
     }
 }
