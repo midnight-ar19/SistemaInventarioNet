c84e0be8005cb2d8dc87d67fe3c2aee173f80a7f
zury <cr0602032024@unab.edu.sv>
2025-04-22 20:21:04 -0600

    Agregando clase ProductoDal - Zury Castillo

---
+++ DAL/ProductoDAL.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ProductoDAL
+    {
+        private readonly InventarioDbContext _db;
+
+        public ProductoDAL(InventarioDbContext db) 
+        {
+            _db = db;
+        }
+
+        //Obtencion de todos productos(listado)
+
+        public List<Producto> ObtenerProductos()
+        {
+            try
+            {
+                return _db.producto.Tolist();
+            }
+            catch (Exception ex)
+            {
+                throw new Eception("Error al obtener productos", ex);
+            }
+
+        }
+        //Agregar producto
+        public void AgregarProducto(Producto producto)
+        {
+            try
+            {
+                _db.productos.Add(producto);
+                _db.SaveChanges();
+            }
+
+            catch (Exception ex) 
+            {
+                throw new Exception("Error al agregar producto", ex);
+            }
+        }
+
+        //Buscar producto
+        public Producto BuscarProducto(int id) 
+        {
+            try 
+            {
+                return _db.productos.Find(id);
+            
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("error al buscar producto", ex);
+            }
+        }
+
+        //actualizando producto
+        public void ActualizarProducto(Producto producto) 
+        {
+            try 
+            {
+                _db.Entry(producto);
+                _db.SaveChanges();
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Error al actualizar producto", ex);
+            }
+
+        }
+
+        //Eliminar producto
+        public void EliminarProducto(int id) 
+        {
+            try 
+            {
+                var producto = _db.productos.find(id);
+                if (producto != null) 
+                {
+                    _db.Productos.Remove(producto);
+                    _db.SaveChanges();
+                
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al eliminar Producto", ex);
+            }
+        }
+            
+
+    }
+
+}
